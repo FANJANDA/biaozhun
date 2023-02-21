9e84e3e0c224febc54cb0c6ebbf11a508fc0d5b7
‘junda’ <junda520ya@gmail.com>
2023-02-21 20:21:17 +0800

    time commit

--- time_learn/1.py
+++ time_learn/1.py
@@ -1,4 +1,22 @@
 import datetime
+import time
 
 res = datetime.datetime.now().replace(microsecond=0) + datetime.timedelta(days=7)
 print(res)
+
+print(time.gmtime())
+
+print(time.strftime('%Y-%m-%d %X'))
+
+t = '2001-07-25 13:00:00'
+res = time.strptime(t, '%Y-%m-%d %X')
+print(res)
+print(time.mktime(res))
+
+print(time.asctime())
+
+print(time.ctime())
+
+print(datetime.datetime.now())
+
+print(datetime.datetime.fromtimestamp(1111))
